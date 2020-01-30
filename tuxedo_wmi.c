b14a2ca5f8348d96a612adbd774bcad76836bb11
Christoffer Sandberg <cs@tuxedo.de>
2020-01-30 10:09:33 +0100

    Module init fix plus cleanup unused functions

--- tuxedo_wmi.c
+++ tuxedo_wmi.c
@@ -1,4 +1,4 @@
-
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
@@ -19,18 +19,6 @@ static int fop_open(struct inode *inode, struct file *file)
 
 static int fop_release(struct inode *inode, struct file *file)
 {
-    return 0;
-}
-
-static ssize_t fop_read(struct file *file, char __user *buf, size_t len, loff_t *off)
-{
-    printk(KERN_INFO "fop_read()\n");
-    return 0;
-}
-
-static ssize_t fop_write(struct file *file, const char __user *buf, size_t len, loff_t *off)
-{
-    printk(KERN_INFO "fop_write()\n");
     return 0;
 }
 
@@ -68,10 +56,10 @@ static long fop_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 
     switch (cmd) {
         case W_FANSPEED:
-            printk(KERN_INFO "W_FANSPEED\n");
-            printk(KERN_INFO "speed param: %ld\n", arg);
+            pr_info("W_FANSPEED\n");
+            pr_info("speed param: %ld\n", arg);
             // copy_from_user(&local_argument, (int32_t *) arg, sizeof(local_argument));
-            printk(KERN_INFO "speed copy: %d\n", argument);
+            pr_info("speed copy: %d\n", argument);
             clevo_wmi_evaluate(CLEVO_WMI_CMD_SET_FANSPEED_VALUE, argument);
             break;
         case W_WEBCAM_SW:
@@ -89,8 +77,6 @@ static long fop_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 
 static struct file_operations fops_dev = {
     .owner              = THIS_MODULE,
-    .read               = fop_read,
-    .write              = fop_write,
     .open               = fop_open,
     .unlocked_ioctl     = fop_ioctl,
     .release            = fop_release,
@@ -105,22 +91,23 @@ static int __init tuxedo_wmi_init(void)
 {
     int err;
 
-    err = alloc_chrdev_region(&tuxedo_wmi_device_handle, 0, 1, "tuxedo_wmi_cdev");
-    if (err != 0) {
-        printk(KERN_INFO "Failed to allocate chrdev region\n");
-        return err;
-    }
-    cdev_init(&tuxedo_wmi_cdev, &fops_dev);
-    if ((cdev_add(&tuxedo_wmi_cdev, tuxedo_wmi_device_handle, 1)) < 0) {
-        printk(KERN_INFO "Failed to add cdev\n");
-    }
-    tuxedo_wmi_device_class = class_create(THIS_MODULE, "tuxedo_wmi");
-    device_create(tuxedo_wmi_device_class, NULL, tuxedo_wmi_device_handle, NULL, "tuxedo_wmi");
-
-    printk(KERN_INFO "WMI Module init.\n");
-    if (!wmi_has_guid(CLEVO_WMI_METHOD_GUID)) {
-        printk(KERN_INFO "GUID (%s) not found\n", CLEVO_WMI_METHOD_GUID);
-        return -ENODEV;
+    if (wmi_has_guid(CLEVO_WMI_METHOD_GUID)) {
+        err = alloc_chrdev_region(&tuxedo_wmi_device_handle, 0, 1, "tuxedo_wmi_cdev");
+        if (err != 0) {
+            pr_err("Failed to allocate chrdev region\n");
+            return err;
+        }
+        cdev_init(&tuxedo_wmi_cdev, &fops_dev);
+        if ((cdev_add(&tuxedo_wmi_cdev, tuxedo_wmi_device_handle, 1)) < 0) {
+            pr_err("Failed to add cdev\n");
+            cdev_del(&tuxedo_wmi_cdev);
+            unregister_chrdev_region(tuxedo_wmi_device_handle, 1);
+        }
+        tuxedo_wmi_device_class = class_create(THIS_MODULE, "tuxedo_wmi");
+        device_create(tuxedo_wmi_device_class, NULL, tuxedo_wmi_device_handle, NULL, "tuxedo_wmi");
+        pr_debug("Successfully initialized\n");
+    } else {
+        pr_debug("Expected GUID (%s) not found, module inactive\n", CLEVO_WMI_METHOD_GUID);
     }
     
     return 0;
@@ -132,7 +119,7 @@ static void __exit tuxedo_wmi_exit(void)
     class_destroy(tuxedo_wmi_device_class);
     cdev_del(&tuxedo_wmi_cdev);
     unregister_chrdev_region(tuxedo_wmi_device_handle, 1);
-    printk(KERN_INFO "WMI Module exit.\n");
+    pr_debug("Module exit\n");
 }
 
 module_init(tuxedo_wmi_init);
