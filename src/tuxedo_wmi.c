ff27a6906621f03cafead4434c7e87836377b177
Christoffer Sandberg <cs@tuxedo.de>
2020-02-03 11:25:20 +0100

    Update version for first package testing

--- src/tuxedo_wmi.c
+++ src/tuxedo_wmi.c
@@ -12,7 +12,7 @@
 
 MODULE_DESCRIPTION("WMI control for TUXEDO laptops");
 MODULE_AUTHOR("TUXEDO Computers <tux@tuxedocomputers.com>");
-MODULE_VERSION("1.0.0");
+MODULE_VERSION("0.1.0");
 MODULE_LICENSE("GPL");
 
 /*static int fop_open(struct inode *inode, struct file *file)
